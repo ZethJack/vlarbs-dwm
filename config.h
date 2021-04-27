2e541ff0ea16e182ab3a53f2b2fb4e36e0f60846
Zeth <zeth@zethjack.eu>
2021-04-27 04:40:13 +0200

    Added systen tray

--- config.h
+++ config.h
@@ -3,6 +3,10 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systraypinning = 1;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
 static const unsigned int gappih    = 1;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 1;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 1;       /* horiz outer gap between windows and screen edge */
