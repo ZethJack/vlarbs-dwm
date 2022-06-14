467bb5ea50e347a992b9a509a2566afc4f0ea0b1
Zeth <zeth@zethjack.eu>
2022-06-14 21:10:27 +0200

    enable smartgaps

--- config.h
+++ config.h
@@ -16,7 +16,7 @@ static const unsigned int gappiv    = 1;       /* vert inner gap between windows
 static const unsigned int gappoh    = 1;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 1;       /* vert outer gap between windows and screen edge */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
